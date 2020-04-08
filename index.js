87d4bef84e902a1303bfb52f70771d9683147785
CharlieHodgkinson <charlotte.hodgkinson4@gmail.com>
2020-04-08 16:46:11 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const core = require('@actions/core');
+const semver = require('semver')
+const execSync = require('child_process').execSync;
+
+try {
+    const localPackage = require(core.getInput('localFilePath'))
+    const packageName = core.getInput('publicPackageName')
+    const publicPackage = execSync('npm view ' + packageName + ' -json', { encoding: 'utf-8' });
+
+    const publicVersion = semver.valid(JSON.parse(publicPackage)["dist-tags"].latest)
+    const localVersion = semver.valid(localPackage.version)
+
+    console.log("public version: ", publicVersion, "local version: ", localVersion)
+
+    semver.gt(localVersion, publicVersion) ? 0 : core.setFailed("version number has not been increased")
+  
+} catch (error) {
+  core.setFailed(error.message);
+}
